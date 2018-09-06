ac955a2f63e1f6a21e66fd2d9a7915364011fd53
deddario <41421507+deddario@users.noreply.github.com>
2018-09-06 19:34:45 +0200

    Add files via upload

---
+++ ZbW.Testing.Dms.Client.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ZbW.Testing.Dms.Client.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZbW.Testing.Dms.Client.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("03a6cd90-ffd7-4211-8c8b-44b4a00c424c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ZbW.Testing.Dms.Client.UnitTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZbW.Testing.Dms.Client.UnitTest
+{
+    [TestClass]
+    public class LoginTest
+    {
+        [TestMethod]
+        public void CmdLogin_CantLogin_ReturnsTrue()
+        {
+            // arrange
+            var onCmdLogin = new LoginTest();
+            //var persistanceService = new PersistanceService(fileAnalyzerStub);
+
+            // act
+            var result = new MainView(Benutzername);
+            // assert
+            Assert.IsFalse(result);
+        }
+    }
+}
